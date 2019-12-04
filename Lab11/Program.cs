48bb8bb149aac5bd109913f634f58a8125ce1701
Vadim <aslgaser@gmail.com>
2019-12-04 21:27:45 +0500

    asd

---
+++ Lab11/Program.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using PrintEditionLibrary;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1();
+            Task2();
+            Task3();
+
+        }
+        public static void AutoFill(ArrayList a, int size)
+        {
+            Random rand = new Random();
+            while (a.Count < size)
+            {
+                Thread.Sleep(20);
+                int r = rand.Next(1, 4);
+                switch (r)
+                {
+                    case 1:
+                        book koob = new book();
+                        a.Add(koob);
+                        break;
+                    case 2:
+                        Textbook uchebnik = new Textbook();
+                        a.Add(uchebnik);
+                        break;
+                    case 3:
+                        Magazine jurnal = new Magazine();
+                        a.Add(jurnal);
+                        break;
+                }
+            }
+        }
+        public static void ShowBookWithMaxPages(ArrayList a)
+        {
+            int max = 0;
+            int index = -1;
+            int count = 0;
+            book f;
+            foreach (PrintEdition item in a)
+            {
+                f = item as book;
+                if (f != null)
+                {
+                    if (f.AmountOfPages > max)
+                    {
+                        max = f.AmountOfPages;
+                        index = count;
+                    }
+                }
+                count++;
+            }
+            if (index >= 0)
+            {
+                Console.WriteLine("Книга с максимальным количеством страниц: \n");
+                book b = a[index] as book;
+                b.Show();
+            }
+            else
+            {
+                Console.WriteLine("В коллекции нет книг");
+            }
+        }
+        public static void ShowAmountOfProgrammingMagazines(ArrayList a)
+        {
+            int count = 0;
+            Magazine p;
+            foreach (PrintEdition item in a)
+            {
+                p = item as Magazine;
+                if (p != null && p.Theme == "Программирование")
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("Количество журналов по программированию: " + count);
+        }
+        public static void ShowEverythingYoungerThan(ArrayList a, int ye)
+        {
+            Console.WriteLine("Все печатные издания новее, чем " + ye + ":\n");
+            int count = 0;
+            foreach (PrintEdition item in a)
+            {
+                if (item.Year > ye)
+                {
+                    item.Show();
+                    count++;
+                }
+            }
+            if (count == 0)
+            {
+                Console.WriteLine("Таких книг нет\n");
+            }
+
+        }
+        public static void ShowItems(ArrayList a)
+        {
+            Console.WriteLine("Все элементы коллекции: \n");
+            foreach (PrintEdition item in a)
+            {
+                item.Show();
+            }
+        }
+        public static void Task1()
+        {
+            ArrayList a = new ArrayList();
+            AutoFill(a, 5);
+            ShowBookWithMaxPages(a);
+            a.Sort();
+            ShowItems(a);
+            Console.ReadKey();
+            Console.Clear();
+            ArrayList aClone = (ArrayList)a.Clone();
+            ShowItems(aClone);
+            Console.ReadKey();
+            Console.Clear();
+            book forSearch = new book(1869, 500, "Поиск элемента в коллекции", "Я");
+            a.Add(forSearch);
+            a.Sort();
+            ShowItems(a);
+            Console.ReadKey();
+            Console.Clear();
+            a.Sort();
+            int removeBook = a.BinarySearch(forSearch);
+            a.RemoveAt(removeBook);
+            ShowItems(a);
+            Console.ReadKey();
+            Console.Clear();
+        }
+        public static void AutoFillDictionary(Dictionary<int, PrintEdition> a, int size)
+        {
+            Random rand = new Random();
+            while (a.Count < size)
+            {
+                Thread.Sleep(20);
+                int r = rand.Next(1, 4);
+                switch (r)
+                {
+                    case 1:
+                        book koob = new book();
+                        a.Add(a.Count + 1, koob);
+                        break;
+                    case 2:
+                        Textbook uchebnik = new Textbook();
+                        a.Add(a.Count + 1, uchebnik);
+                        break;
+                    case 3:
+                        Magazine jurnal = new Magazine();
+                        a.Add(a.Count + 1, jurnal);
+                        break;
+                }
+            }
+        }
+        public static void ShowDictionaryItems(Dictionary<int, PrintEdition> a)
+        {
+            foreach (KeyValuePair<int, PrintEdition> keyValue in a)
+            {
+                Console.WriteLine(keyValue.Key + "\n");
+                keyValue.Value.Show();
+            }
+            Console.ReadKey();
+            Console.Clear();
+        }
+        public static void SEYTInDictionary(Dictionary<int, PrintEdition> a, int ye)
+        {
+            Console.WriteLine("Все печатные издания новее, чем " + ye + ":\n");
+            int count = 0;
+            foreach (KeyValuePair<int, PrintEdition> keyValue in a)
+            {
+                if (keyValue.Value.Year > ye)
+                {
+                    keyValue.Value.Show();
+                    count++;
+                }
+            }
+            if (count == 0)
+            {
+                Console.WriteLine("Таких книг нет\n");
+            }
+            Console.ReadKey();
+            Console.Clear();
+        }
+        public static void ShowDictionarySorted(Dictionary<int, PrintEdition> a)
+        {
+            var sortedDict = new SortedDictionary<int, PrintEdition>(a);
+            foreach (var kvp in sortedDict)
+            {
+                Console.WriteLine(kvp.Key + "\n");
+                kvp.Value.Show();
+            }
+            Console.ReadKey();
+            Console.Clear();
+        }
+        public static void SearchByKey(Dictionary<int, PrintEdition> a,int key)
+        {
+            PrintEdition print;
+            a.TryGetValue(key,out print);
+            if (print != null)
+            {
+                Console.WriteLine("Найденный объект: \n" );
+                print.Show();
+            }
+            else
+            {
+                Console.WriteLine("Объект не найден");
+            }
+        }
+        public static void Task2()
+        {
+            Dictionary<int, PrintEdition> library = new Dictionary<int, PrintEdition>();
+            AutoFillDictionary(library, 5);
+            ShowDictionaryItems(library);
+            SEYTInDictionary(library, 1500);
+            Dictionary<int, PrintEdition> newLibrary = new Dictionary<int, PrintEdition>(library);
+            Magazine journal = new Magazine(2019, 100, 12, "Журнал");
+            newLibrary.Add(-5, journal);
+            ShowDictionarySorted(newLibrary);
+            SearchByKey(newLibrary, 1);
+        }
+        public static void Task3()
+        {
+            TestCollections a = new TestCollections(10);
+            a.MyListTimeTest();
+            Console.ReadKey();
+            Console.Clear();
+            a.myListStringTest();
+            Console.ReadKey();
+            Console.Clear();
+            a.MyDictionaryTest();
+            Console.ReadKey();
+            Console.Clear();
+            a.MyDictionaryStringTest();
+        }
+
+    }
+}
---
+++ Lab11/TestCollections.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PrintEditionLibrary;
+using System.Diagnostics;
+using System.Threading;
+namespace Lab11
+{
+    class TestCollections
+    {
+        List<PrintEdition> myList = new List<PrintEdition>();
+        List<string> myListString = new List<string>();
+        Dictionary<PrintEdition, book> myDictionary = new Dictionary<PrintEdition, book>();
+        Dictionary<string, book> myDictionaryString = new Dictionary<string, book>();
+        public TestCollections(int size)
+        {
+            for(int i = 0; i < size; i++)
+            {
+                book t = new book();
+                myList.Add(t.ReturnBase);
+                myListString.Add(t.ReturnBase.ToString());
+                myDictionary.Add(myList.Last(), t);
+                myDictionaryString.Add(myListString.Last(), t);
+                Thread.Sleep(20);
+            }
+        }
+        public void MyListTimeTest()
+        {
+            PrintEdition first, middle, last, nonexistant;
+            first = myList.First();
+            long[] testtimes = new long[4];
+            int index = 0;
+            for (int i = 0; i < myList.Count / 2; i++)
+            {
+                index++;
+            }
+            middle = myList[index];
+            last = myList.Last();
+            nonexistant = new PrintEdition(2220, -1);
+            Stopwatch tt = new Stopwatch();
+            tt.Start();
+            if (myList.Contains(first))
+            {
+                Console.WriteLine();
+            }
+            tt.Stop();
+            testtimes[0] = tt.ElapsedTicks;
+            tt.Restart();
+            if (myList.Contains(middle))
+            {
+                Console.WriteLine();
+            }
+            tt.Stop();
+            testtimes[1] = tt.ElapsedTicks;
+            tt.Restart();
+            if (myList.Contains(last))
+            {
+                Console.WriteLine();
+            }
+            tt.Stop();
+            testtimes[2] = tt.ElapsedTicks;
+            if (!myList.Contains(nonexistant))
+            {
+                Console.WriteLine();
+            }
+            tt.Stop();
+            testtimes[3] = tt.ElapsedTicks;
+            Console.WriteLine("Время поиска в List<PrintEdition>\n\nВремя поиска первого элемента: " + testtimes[0] + "\nВремя поиска центрального элемента: " + testtimes[1] +
+                "\nВремя поиска последнего элемента: " + testtimes[2] + "\nВремя поиска не существующего элемента: " + testtimes[3]);
+
+        }
+        public void myListStringTest()
+        {
+            string first, middle, last, nonexistant;
+            first = myListString.First();
+            long[] testtimes = new long[4];
+            int index = 0;
+            for (int i = 0; i < myList.Count / 2; i++)
+            {
+                index++;
+            }
+            middle = myListString[index];
+            last = myListString.Last();
+            nonexistant = "не существует";
+            Stopwatch tt = new Stopwatch();
+            tt.Start();
+            if (myListString.Contains(first))
+            {
+                Console.WriteLine();
+            }
+            tt.Stop();
+            testtimes[0] = tt.ElapsedTicks;
+            tt.Restart();
+            if (myListString.Contains(middle))
+            {
+                Console.WriteLine();
+            }
+            tt.Stop();
+            testtimes[1] = tt.ElapsedTicks;
+            tt.Restart();
+            if (myListString.Contains(last))
+            {
+                Console.WriteLine();
+            }
+            tt.Stop();
+            testtimes[2] = tt.ElapsedTicks;
+            if (!myListString.Contains(nonexistant))
+            {
+                Console.WriteLine();
+            }
+            tt.Stop();
+            testtimes[3] = tt.ElapsedTicks;
+            Console.WriteLine("Время поиска в List<String>\n\nВремя поиска первого элемента: " + testtimes[0] + "\nВремя поиска центрального элемента: " + testtimes[1] +
+                "\nВремя поиска последнего элемента: " + testtimes[2] + "\nВремя поиска не существующего элемента: " + testtimes[3]);
+        }
+        public void MyDictionaryTest()
+        {
+            PrintEdition first = new PrintEdition(), middle = new PrintEdition(), last = new PrintEdition(), nonexistant = new PrintEdition(2222,-1);
+            long[] testtimes = new long[4];
+            int index = 0;
+            foreach (var item in myDictionary)
+            {
+                if (index == 0)
+                {
+                    first = item.Key;
+                }
+                if (index == myDictionary.Count / 2 - 1)
+                {
+                    middle = item.Key;
+                }
+                if (index == myDictionary.Count - 1)
+                {
+                    last = item.Key;
+                }
+                index++;
+            }
+            nonexistant = new PrintEdition(2220, -1);
+            Stopwatch tt = new Stopwatch();
+            tt.Start();
+            if (myDictionary.ContainsKey(first))
+            {
+                Console.WriteLine();
+            }
+            tt.Stop();
+            testtimes[0] = tt.ElapsedTicks;
+            tt.Restart();
+            if (myDictionary.ContainsKey(middle))
+            {
+                Console.WriteLine();
+            }
+            tt.Stop();
+            testtimes[1] = tt.ElapsedTicks;
+            tt.Restart();
+            if (myDictionary.ContainsKey(last))
+            {
+                Console.WriteLine();
+            }
+            tt.Stop();
+            testtimes[2] = tt.ElapsedTicks;
+            if (!myDictionary.ContainsKey(nonexistant))
+            {
+                Console.WriteLine();
+            }
+            tt.Stop();
+            testtimes[3] = tt.ElapsedTicks;
+            Console.WriteLine("Время поиска по ключу в Dictionary<PrintEdition,book>\n\nВремя поиска первого элемента: " + testtimes[0] + "\nВремя поиска центрального элемента: " + testtimes[1] +
+                "\nВремя поиска последнего элемента: " + testtimes[2] + "\nВремя поиска не существующего элемента: " + testtimes[3]);
+        }
+        public void MyDictionaryStringTest()
+        {
+            book first = new book(),middle = new book(), last = new book(), nonexistant = new book(2222,-1,"","");
+            long[] testtimes = new long[4];
+            int index = 0;
+            foreach (var item in myDictionaryString)
+            {
+                if (index == 0)
+                {
+                    first = item.Value;
+                }
+                if (index == myDictionary.Count / 2 - 1)
+                {
+                    middle = item.Value;
+                }
+                if (index == myDictionary.Count - 1)
+                {
+                    last = item.Value;
+                }
+                index++;
+            }
+            Stopwatch tt = new Stopwatch();
+            tt.Start();
+            if (myDictionaryString.ContainsValue(first))
+            {
+                Console.WriteLine();
+            }
+            tt.Stop();
+            testtimes[0] = tt.ElapsedTicks;
+            tt.Restart();
+            if (myDictionaryString.ContainsValue(middle))
+            {
+                Console.WriteLine();
+            }
+            tt.Stop();
+            testtimes[1] = tt.ElapsedTicks;
+            tt.Restart();
+            if (myDictionaryString.ContainsValue(last))
+            {
+                Console.WriteLine();
+            }
+            tt.Stop();
+            testtimes[2] = tt.ElapsedTicks;
+            if (!myDictionaryString.ContainsValue(nonexistant))
+            {
+                Console.WriteLine();
+            }
+            tt.Stop();
+            testtimes[3] = tt.ElapsedTicks;
+            Console.WriteLine("Время поиска по значению в Dictionary<string,book>\n\nВремя поиска первого элемента: " + testtimes[0] + "\nВремя поиска центрального элемента: " + testtimes[1] +
+                "\nВремя поиска последнего элемента: " + testtimes[2] + "\nВремя поиска не существующего элемента: " + testtimes[3]);
+        }
+    }
+
+}
